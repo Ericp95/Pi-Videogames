5995b809bbe95587bb8d62e367b9507a1352b718
Ericp95 <ericp6960@gmail.com>
2022-10-11 10:27:11 -0300

    Update Videogame.js

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,40 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    released: {
+      type: DataTypes.STRING
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    img: {
+      type: DataTypes.TEXT
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN, 
+      allowNull: false,
+      defaultValue: true
+    }
+  },
+  {
+    timestamps: false
   });
 };
