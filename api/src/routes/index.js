1d63f639b109c5ea65931a7279497560931a4272
Ericp95 <ericp6960@gmail.com>
2022-10-25 11:07:17 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,17 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
+const{ getGenres }=require('../functions/Genres')
+const{ getVIdeogamesId , getVideogames, addedVideogames}=require('../functions/Videogames')
+const{ PostVideogame }=require('../functions/Postvideogame')
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get('/genres', getGenres);
+router.get('/videogames/:id', getVIdeogamesId);
+router.post('/videogame', PostVideogame);
+router.get('/videogames', getVideogames);
 
 module.exports = router;
