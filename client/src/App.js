0e3e23b114b66c080f67458c4b31b5b7006538e8
Ericp95 <ericp6960@gmail.com>
2022-10-27 11:28:33 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
+import Home from './Components/Home/home';
+import Landingpage from './Components/Landingpage/landingpage';
+import Detail from './Components/Detail/detail';
+import CreateVideogame from './Components/CreateVideogame/createvideogame'
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+        <Route exact path="/" component={Landingpage}/>
+        <Route  exact path="/home"   component={Home} />
+        <Route  exact path="/home/:id"   component={Detail} />
+        <Route  exact path="/createvideogame"   component={CreateVideogame} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
